6061a3a10debcd86b3c5c0195cc77737c430669d
Swarnim Rai <62171065+swarnimstrange@users.noreply.github.com>
2021-09-07 16:54:24 +0530

    Update Project.js

--- src/Project.js
+++ src/Project.js
@@ -52,8 +52,8 @@ class Project extends Component{
                             <h4>Comics App</h4>
                             <p>Flutter and Firebase App Development</p>
                             <div className="portfolio-links">
-                                <a href="https://www.dccomics.com/sites/default/files/mustRead_DCC_5_v2_5f6b8b49bfd6a9.60761869.jpg" data-gall="portfolioGallery" className="venobox" title="Comics App"><i className="bx bx-plus"></i></a>
-                                <a href="https://github.com/swarnimstrange/Flutter_Apps/tree/master/Comics_App"><i className="bx bx-link"></i></a>
+                                <a href="https://www.dccomics.com/sites/default/files/mustRead_DCC_5_v2_5f6b8b49bfd6a9.60761869.jpg" target="_blank" rel="noreferrer noopener"><i className="bx bx-plus"></i></a>
+                                <a href="https://github.com/swarnimstrange/Flutter_Apps/tree/master/Comics_App" target="_blank" rel="noreferrer noopener"><i className="bx bx-link"></i></a>
                             </div>
                             </div>
                         </div>
@@ -66,8 +66,8 @@ class Project extends Component{
                             <h4>ChatBot for petshop</h4>
                             <p>ML and AI </p>
                             <div className="portfolio-links">
-                                <a href="https://miro.medium.com/max/800/0*oz2e-hQtsHOWzoB4." data-gall="portfolioGallery" className="venobox" title="Pet Order ChatBot"><i className="bx bx-plus"></i></a>
-                                <a href="https://console.dialogflow.com/api-client/demo/embedded/a7cedbd3-2a25-4aeb-b90f-5e096392b176"><i className="bx bx-link"></i></a>
+                                <a href="https://miro.medium.com/max/800/0*oz2e-hQtsHOWzoB4." target="_blank" rel="noreferrer noopener"><i className="bx bx-plus"></i></a>
+                                <a href="https://console.dialogflow.com/api-client/demo/embedded/a7cedbd3-2a25-4aeb-b90f-5e096392b176" target="_blank" rel="noreferrer noopener"><i className="bx bx-link"></i></a>
                             </div>
                             </div>
                         </div>
@@ -80,8 +80,8 @@ class Project extends Component{
                             <h4>Recommendation Systems</h4>
                             <p>ML recommendation of Mangas and Movies</p>
                             <div className="portfolio-links">
-                            <a href="https://dw9to29mmj727.cloudfront.net/properties/2016/1880-SeriesThumbnailsManga_DemonSlayer_400x320.jpg" data-gall="portfolioGallery" className="venobox" title="Manga Recommendation"><i className="bx bx-plus"></i></a>
-                            <a href="https://github.com/swarnimstrange/Recommendation_Systems"><i className="bx bx-link"></i></a>
+                            <a href="https://dw9to29mmj727.cloudfront.net/properties/2016/1880-SeriesThumbnailsManga_DemonSlayer_400x320.jpg" target="_blank" rel="noreferrer noopener"><i className="bx bx-plus"></i></a>
+                            <a href="https://github.com/swarnimstrange/Recommendation_Systems" target="_blank" rel="noreferrer noopener"><i className="bx bx-link"></i></a>
                             </div>
                             </div>
                         </div>
@@ -94,8 +94,8 @@ class Project extends Component{
                             <h4>Mask Detection</h4>
                             <p>Computer Vision </p>
                             <div className="portfolio-links">
-                            <a href="https://5.imimg.com/data5/PI/FD/NK/SELLER-5866466/images-500x500.jpg" data-gall="portfolioGallery" className="venobox" title="Detect Masks"><i className="bx bx-plus"></i></a>
-                                <a href="https://github.com/swarnimstrange/TSF/tree/main/Mask_Detection"><i className="bx bx-link"></i></a>
+                            <a href="https://5.imimg.com/data5/PI/FD/NK/SELLER-5866466/images-500x500.jpg" target="_blank" rel="noreferrer noopener"><i className="bx bx-plus"></i></a>
+                                <a href="https://github.com/swarnimstrange/TSF/tree/main/Mask_Detection" target="_blank" rel="noreferrer noopener"><i className="bx bx-link"></i></a>
                             </div>
                             </div>
                         </div>
@@ -108,8 +108,8 @@ class Project extends Component{
                             <h4>Virat Kohli T20 score Predictor</h4>
                             <p>ML and Flask </p>
                             <div className="portfolio-links">
-                            <a href="https://english.cdn.zeenews.com/sites/default/files/2016/04/29/483345-virat-kohli-back-shot.jpg" data-gall="portfolioGallery" className="venobox" title="Predict Virat Kohli T20 Score"><i className="bx bx-plus"></i></a>
-                                <a href="https://github.com/swarnimstrange/virat20"><i className="bx bx-link"></i></a>
+                            <a href="https://english.cdn.zeenews.com/sites/default/files/2016/04/29/483345-virat-kohli-back-shot.jpg" target="_blank" rel="noreferrer noopener"><i className="bx bx-plus"></i></a>
+                                <a href="https://github.com/swarnimstrange/virat20" target="_blank" rel="noreferrer noopener"><i className="bx bx-link"></i></a>
                             </div>
                             </div>
                         </div>
@@ -122,8 +122,8 @@ class Project extends Component{
                             <h4>Space Exploration and Learning Website for Children</h4>
                             <p>Website making Blogger </p>
                             <div className="portfolio-links">
-                                <a href="https://c4.wallpaperflare.com/wallpaper/407/412/20/beautiful-space-stars-planets-cosmos-wallpaper-preview.jpg" data-gall="portfolioGallery" className="venobox" title="Explore Space and Learn"><i className="bx bx-plus"></i></a>
-                                <a href="https://iris-space-team.blogspot.com/2020/09/blog-post.html"><i className="bx bx-link"></i></a>
+                                <a href="https://c4.wallpaperflare.com/wallpaper/407/412/20/beautiful-space-stars-planets-cosmos-wallpaper-preview.jpg" target="_blank" rel="noreferrer noopener"><i className="bx bx-plus"></i></a>
+                                <a href="https://iris-space-team.blogspot.com/2020/09/blog-post.html" target="_blank" rel="noreferrer noopener"><i className="bx bx-link"></i></a>
                             </div>
                             </div>
                         </div>
